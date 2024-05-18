22ce480686cef0494eb0afa070c2fdd2dc1c273d
samandar <samandarnazarov07@gmail.com>
2024-05-18 07:03:35 +0500

    yangi page qoshdim intut oxiri qoldi

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,15 +5,16 @@ urlpatterns = [
     path('',index, name='index'),
    
     path('yangiliklar/',news,name='news'),
-    path('yangiliklar/<int:pk>/',category_list, name='category_list'),
     path('yangiliklar/<int:pk>/',news_detail,name='ArticleNews_details'),
    
     path('elonlar/',elon,name='elon'),
-    path('elon/<int:pk>/',category_elon, name='category_elon'),
     path('elon/<int:pk>/',elon_detail,name='ArticleElon_detail'),
     
     
     path('institut',institut, name='institut'),
+    path('kengashlar',kengash, name='kengash'),
+    path('meyoriy-hujjatlar',meyoriyHujjat, name='meyoriy-hujjat'),
+    
     path('institut/institut-nizomi',institutPage_1, name='ins-nizomi'),
     path('institut/institut-tarixi',institutPage_2, name='ins-tarixi'),
     path('institut/institut-strukturasi',institutPage_3, name='ins-strukturasi'),
@@ -21,6 +22,31 @@ urlpatterns = [
     path('institut/rekivizitlar',institutPage_5, name='rekivizitlar'),
     path('institut/moddiy-texnik-baza',institutPage_6, name='md-baza'),
     path('institut/sport-inshoatlari',institutPage_7, name='sp-inshoatlari'),
+    
+    
+    path('institut/kengash/OTM-kengash',OTMkengash, name='OTM-kengash'),
+    path('institut/kengash/OTM-kengash/kengash-2019-2020',kengash1920, name='kengash1920'),
+    path('institut/kengash/OTM-kengash/kengash-2020-2021',kengash2021, name='kengash2021'),
+    path('institut/kengash/OTM-kengash/kengash-2021-2022',kengash2122, name='kengash2122'),
+    path('institut/kengash/OTM-kengash/kengash-2022-2023',kengash2223, name='kengash2223'),
+    path('institut/kengash/OTM-kengash/kengash-2023-2024',kengash2324, name='kengash2324'),
+    
+    
+    path('institut/kengash/Vasiylik',vasiylik, name='vasiylik'),
+    path('institut/kengash/Vasiylik/namunaviy-nizom',vp1, name='namunaviy-nizom'),
+    path('institut/kengash/Vasiylik/Vasiylik-kengashi-tarkibi',vp2, name='vasiylik-kt'),
+    
+    
+    path('institut/kengash/Yosh-olimalr-kengashi',yoshOlimlar, name='yoshOlimlar'),
+    path('institut/kengash/Xotin-Qizlar-kengashi',xotinQizlar, name='xotinQizlar'),
+    path('institut/kengash/Ilmiy-kengash',ilmiyKengash, name='ilmiyKengash'),
+    
+    
+    path('institut/meyoriy-hujjatlar/prezident-qarorlari',mhPage1, name='mh-page-1'),
+    path('institut/meyoriy-hujjatlar/prezident-farmonlari',mhPage2, name='mh-page-2'),
+    path('institut/meyoriy-hujjatlar/vazirlik-buyruqlari',mhPage3, name='mh-page-3'),
+    path('institut/meyoriy-hujjatlar/adliya-qarorlari',mhPage4, name='mh-page-4'),
+    
     
     
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -25,25 +25,6 @@ def index(request):
 
 
 
-def category_list(request,pk):
-    Category_news = CategoryNews.objects.all()
-    Article_news = ArticleNews.objects.all()
-    context = {
-        'Category_news':Category_news,
-        'Article_news':Article_news,  
-    }
-    return render(request,'news/news.html',context)
-
-def category_elon(request,pk):
-    Category_elon = CategoryElon.objects.all()
-    Article_elon = ArticleElon.objects.all()
-    context = {
-        'Category_elon':Category_elon,
-        'Article_elon':Article_elon, 
-    }
-    return render(request,'news/elon.html',context)
-
-
 def news(request):
     Category_news = CategoryNews.objects.all()
     Article_news = ArticleNews.objects.all()
@@ -80,11 +61,19 @@ def elon_detail(request,pk):
     }
     return render(request,'news/elon-in.html',context)
 
+
+
 ################################################################################################
 # Instut hamma pagelari
 
 def institut(request):
-    return render(request,'institut/institut.html')
+    return render(request,'institut/__institut.html')
+
+def kengash(request):
+    return render(request,'institut/__kengash.html')
+
+def meyoriyHujjat(request):
+    return render(request,'institut/__meyoriy-hujjat.html')
 
 def institutPage_1(request):
     return render(request,'institut/instut-page1.html')
@@ -108,3 +97,59 @@ def institutPage_7(request):
     return render(request,'institut/instut-page7.html')
 
 ################################################################################################
+
+def OTMkengash(request):
+    return render(request,'institut/__kengash-in.html')
+
+def kengash1920(request):
+    return render(request,'institut/kengash_19-20.html')
+
+def kengash2021(request):
+    return render(request,'institut/kengash_20-21.html')
+
+def kengash2122(request):
+    return render(request,'institut/kengash_21-22.html')
+
+def kengash2223(request):
+    return render(request,'institut/kengash_22-23.html')
+
+def kengash2324(request):
+    return render(request,'institut/kengash_23-24.html')
+
+################################################################################################
+
+def vasiylik(request):
+    return render(request,"institut/__vasiylik.html")
+
+def vp1(request):
+    return render(request,"institut/vasiylik-page-1.html")
+
+def vp2(request):
+    return render(request,"institut/vasiylik-page-2.html")
+
+
+################################################################################################
+
+def yoshOlimlar(request):
+    return render(request,"institut/__yosh-olimlar.html")
+
+def xotinQizlar(request):
+    return render(request,"institut/__xotin-qizlar.html")
+
+def ilmiyKengash(request):
+    return render(request,"institut/__ilmiy-kengash.html")
+
+################################################################################################
+
+def mhPage1(request):
+    return render(request,'institut/mh-page1.html')
+
+def mhPage2(request):
+    return render(request,'institut/mh-page2.html')
+
+def mhPage3(request):
+    return render(request,'institut/mh-page3.html')
+
+def mhPage4(request):
+    return render(request,'institut/mh-page4.html')
+
