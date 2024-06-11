f597fa69953d58ad06a21a2cec7b083591da406b
mrjohn7751 <jahongirberdiyev7751@gmail.com>
2024-06-11 22:24:03 +0500

    yanglik qoshildi

--- myapp/models.py
+++ myapp/models.py
@@ -83,9 +83,9 @@ class ArticleElon(models.Model):
     title = models.CharField( max_length=200, verbose_name='Sarlovha')
     body = models.TextField(verbose_name='Tana qismi')
     img = models.ImageField(upload_to='Photos/Yangiliklar/',)
-    img1 = models.ImageField(blank=True, default=True, upload_to='Photos/Yangiliklar/',)
-    img2 = models.ImageField(blank=True, default=True, upload_to='Photos/Yangiliklar/',)
-    img3 = models.ImageField(blank=True, default=True, upload_to='Photos/Yangiliklar/',)
+    img1 = models.ImageField(blank=True, upload_to='Photos/Yangiliklar/',)
+    img2 = models.ImageField(blank=True, upload_to='Photos/Yangiliklar/',)
+    img3 = models.ImageField(blank=True, upload_to='Photos/Yangiliklar/',)
     created_at = models.DateTimeField( auto_now_add=True)
     
     def __str__(self):
